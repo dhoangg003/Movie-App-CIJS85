90faed2d1dc9e3803f4ed6f3ee5ddc214e1033bd
dhoangg003 <duonghoang0363181030@gmail.com>
2023-07-28 23:58:53 +0700

    update sidebar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,33 +1,48 @@
 import React from 'react'
 import "./SideBar.css";
+import logo from"../../images/logo.jpg"
+import {FaHome} from "react-icons/fa";
+import {FaFilm} from "react-icons/fa";
+import {FaTv} from "react-icons/fa";
+import {FaStar} from "react-icons/fa";
+import {FaList} from "react-icons/fa";
+import {FaThumbsUp} from "react-icons/fa";
+import {FaHeart} from "react-icons/fa";
+import {FaHandPointUp} from "react-icons/fa";
+import {FaCog} from "react-icons/fa";
+import {FaUserPlus} from "react-icons/fa";
+import {FaArrowRight} from "react-icons/fa";
+
+
+
 
 const SideBar = () => {
     return (
         <div className='sidebar-wrapper'>
             <div className='sidebar-avatar'>
-                <p>logo ne</p>
-                <p>ten</p>
+                <img src={logo} alt="logo" />
+
             </div>
             <div className='sidebar'>
                 <ul>
-                    <li><i class="fas fa-home"></i> Home</li>
-                    <li><i class="fas fa-film"></i> Movies</li>
-                    <li><i class="fas fa-tv"></i> Shows</li>
-                    <li><i class="fas fa-star"></i> Rated</li>
-                    <li><i class="fas fa-list"></i> Genre</li>
-                    <li><i class="fas fa-list-alt"></i> Playlist</li>
+                    <li className="icon-with-text"><FaHome/> Home</li>
+                    <li className="icon-with-text"><FaFilm/>Movies</li>
+                    <li className="icon-with-text"><FaTv/> Shows</li>
+                    <li className="icon-with-text"><FaStar/> Rated</li>
+                    <li className="icon-with-text"><FaList/> Genre</li>
+                    <li className="icon-with-text"><FaList/> Playlist</li>
                 </ul>
 
                 <ul>
-                    <li><i class="fas fa-thumbs-up"></i> Likes</li>
-                    <li><i class="fas fa-heart"></i> Favourite</li>
-                    <li><i class="fas fa-user"></i> For You</li>
+                    <li className="icon-with-text"><FaThumbsUp/> Likes</li>
+                    <li className="icon-with-text"><FaHeart/> Favourite</li>
+                    <li className="icon-with-text"><FaHandPointUp/> For You</li>
                 </ul>
 
                 <ul>
-                    <li><i class="fas fa-cog"></i> Setting</li>
-                    <li><i class="fas fa-user-plus"></i> Membership</li>
-                    <button class="btn2">Sign Out</button>
+                    <li className="icon-with-text"><FaCog/> Setting</li>
+                    <li className="icon-with-text" ><FaUserPlus/> Member</li>
+                    <button className="btn2"><FaArrowRight/>Sign Out</button>
                 </ul>
             </div>
         </div>
