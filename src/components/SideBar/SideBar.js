fb164b5724b2cb1ef74ab69dae0d2d8630d2a0b6
duylinh13 <huynhleduylinh1311@gmail.com>
2023-07-31 21:06:03 +0700

    add css sideber

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,52 +1,75 @@
-import React from 'react'
+import React from "react";
 import "./SideBar.css";
-import logo from"../../images/logo.jpg"
-import {FaHome} from "react-icons/fa";
-import {FaFilm} from "react-icons/fa";
-import {FaTv} from "react-icons/fa";
-import {FaStar} from "react-icons/fa";
-import {FaList} from "react-icons/fa";
-import {FaThumbsUp} from "react-icons/fa";
-import {FaHeart} from "react-icons/fa";
-import {FaHandPointUp} from "react-icons/fa";
-import {FaCog} from "react-icons/fa";
-import {FaUserPlus} from "react-icons/fa";
-import {FaArrowRight} from "react-icons/fa";
-
-
-
+import logo from "../../images/logo.jpg";
+import { FaHome } from "react-icons/fa";
+import { FaFilm } from "react-icons/fa";
+import { FaTv } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+import { FaList } from "react-icons/fa";
+import { FaThumbsUp } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import { FaHandPointUp } from "react-icons/fa";
+import { FaCog } from "react-icons/fa";
+import { FaUserPlus } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
+import "typeface-roboto";
 
 const SideBar = () => {
-    return (
-        <div className='sidebar-wrapper'>
-            <div className='sidebar-avatar'>
-                <img src={logo} alt="logo" />
+  return (
+    <div className="sidebar-wrapper">
+      <div className="sidebar-avatar">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="sidebar">
+        <ul>
+          <li className="icon-with-text">
+            <FaHome /> Home
+          </li>
+          <li className="icon-with-text">
+            <FaFilm />
+            Movies
+          </li>
+          <li className="icon-with-text">
+            <FaTv /> Shows
+          </li>
+          <li className="icon-with-text">
+            <FaStar /> Rated
+          </li>
+          <li className="icon-with-text">
+            <FaList /> Genre
+          </li>
+          <li className="icon-with-text">
+            <FaList /> Playlist
+          </li>
+        </ul>
 
-            </div>
-            <div className='sidebar'>
-                <ul>
-                    <li className="icon-with-text"><FaHome/> Home</li>
-                    <li className="icon-with-text"><FaFilm/>Movies</li>
-                    <li className="icon-with-text"><FaTv/> Shows</li>
-                    <li className="icon-with-text"><FaStar/> Rated</li>
-                    <li className="icon-with-text"><FaList/> Genre</li>
-                    <li className="icon-with-text"><FaList/> Playlist</li>
-                </ul>
+        <ul>
+          <li className="icon-with-text">
+            <FaThumbsUp /> Likes
+          </li>
+          <li className="icon-with-text">
+            <FaHeart /> Favourite
+          </li>
+          <li className="icon-with-text">
+            <FaHandPointUp /> For You
+          </li>
+        </ul>
 
-                <ul>
-                    <li className="icon-with-text"><FaThumbsUp/> Likes</li>
-                    <li className="icon-with-text"><FaHeart/> Favourite</li>
-                    <li className="icon-with-text"><FaHandPointUp/> For You</li>
-                </ul>
+        <ul>
+          <li className="icon-with-text">
+            <FaCog /> Setting
+          </li>
+          <li className="icon-with-text">
+            <FaUserPlus /> Member
+          </li>
+          <button className="btn2">
+            <FaArrowRight />
+            Sign Out
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-                <ul>
-                    <li className="icon-with-text"><FaCog/> Setting</li>
-                    <li className="icon-with-text" ><FaUserPlus/> Member</li>
-                    <button className="btn2"><FaArrowRight/>Sign Out</button>
-                </ul>
-            </div>
-        </div>
-    )
-}
-
-export default SideBar
+export default SideBar;
