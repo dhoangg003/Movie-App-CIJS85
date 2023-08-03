a2e568ed97e202c45c57e5f923c1e1e26c711847
duylinh13 <huynhleduylinh1311@gmail.com>
2023-08-03 20:12:52 +0700

    css

--- src/components/SectionHero/SectionHero.js
+++ src/components/SectionHero/SectionHero.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import Slider1 from "../../images/slider1.jpg";
 import Slider2 from "../../images/slider2.jpg";
-import Slider3 from "../../images/slider3.jpg";
+import Slider4 from "../../images/slider4.jpg";
 import "./SectionHero.css";
 
 export default class SectionHero extends Component {
@@ -13,6 +13,8 @@ export default class SectionHero extends Component {
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
     };
     return (
       <div>
@@ -26,7 +28,7 @@ export default class SectionHero extends Component {
             <img className="img" src={Slider2} />
           </div>
           <div>
-            <img className="img" src={Slider3} />
+            <img className="img" src={Slider4} />
           </div>
         </Slider>
       </div>
